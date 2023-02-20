19c4cad655eab22a66e5cf91f59457516db749d9
abhishekcc08 <abhishekcc08@gmail.com>
2023-02-20 09:32:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import menuApi from "./component/Basics/menuApi";
+import Resturant from "./component/Basics/Resturant";
+// import MenuCard from "./component/Hooks/MenuCard";
+// import Navbar from "./component/Hooks/Navbar";
+// import UseReducer from "./component/Hooks/useReducer";
+
+const App = () => {
+  return (<div>
+
+  <Resturant />
+
+  </div>
+)};
+
+export default App;
